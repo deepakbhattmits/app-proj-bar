0194ac97f74cc37fbfdeec66a445caaf5f410338
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-20 22:45:27 +0530

    refector

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -1,57 +1,56 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import MainComponent from "../components/MainComponent";
-import { getBeers, addTofavorites, removeFromfavorites } from "../actions";
-import SearchPage from "./SearchPage";
-class MainPage extends Component {
-  componentDidMount() {
-    if (!this.props.beers) {
-      this.props.getBeers(); // call action
-    }
-  }
-  getDrivedStateFromProps() {
-    this.getfav();
-  }
-  getfav = () => {
-    return this.props.fbeers;
-  };
-  addToFavorite = e => {
-    this.props.addTofavorites(e.target.id);
-  };
-  removeFromfavorites = e => {
-    this.props.removeFromfavorites(e.target.id);
-  };
+/** @format */
 
-  render() {
-    return (
-      <Fragment>
-        <SearchPage />
-        <div className="ui row">
-          <MainComponent
-            beers={this.props.beers}
-            fbeers={this.getfav()}
-            addToFavorite={this.addToFavorite}
-            removeFromfavorites={this.removeFromfavorites}
-            className={this.props.favorites}
-          />
-        </div>
-      </Fragment>
-    );
-  }
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import MainComponent from '../components/MainComponent';
+import { getBeers, addTofavorites, removeFromfavorites } from '../actions';
+import SearchPage from './SearchPage';
+class MainPage extends Component {
+	componentDidMount() {
+		if (!this.props.beers) {
+			this.props.getBeers(); // call action
+		}
+	}
+	getDrivedStateFromProps() {
+		this.getfav();
+	}
+	getfav = () => {
+		return this.props.fbeers;
+	};
+	addToFavorite = e => {
+		this.props.addTofavorites(e.target.id);
+	};
+	removeFromfavorites = e => {
+		this.props.removeFromfavorites(e.target.id);
+	};
+
+	render() {
+		return (
+			<Fragment>
+				<SearchPage />
+				<div className='ui row'>
+					<MainComponent
+						beers={this.props.beers}
+						fbeers={this.getfav()}
+						addToFavorite={this.addToFavorite}
+						removeFromfavorites={this.removeFromfavorites}
+						className={this.props.favorites}
+					/>
+				</div>
+			</Fragment>
+		);
+	}
 }
 const mapDispatchToProps = dispatch => ({
-  getBeers: () => dispatch(getBeers()),
-  addTofavorites: data => dispatch(addTofavorites(data)),
-  removeFromfavorites: data => dispatch(removeFromfavorites(data))
+	getBeers: () => dispatch(getBeers()),
+	addTofavorites: data => dispatch(addTofavorites(data)),
+	removeFromfavorites: data => dispatch(removeFromfavorites(data))
 });
 const mapStateToProps = state => {
-  return {
-    beers: state.beerData.beers,
-    fbeers: state.beerData.addFbeers,
-    favorites: state.beerData.favorites
-  };
+	return {
+		beers: state.beerData.beers,
+		fbeers: state.beerData.addFbeers,
+		favorites: state.beerData.favorites
+	};
 };
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(MainPage);
+export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
