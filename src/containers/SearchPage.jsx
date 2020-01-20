241e246fc52c3bfef5b7d4810a7a30b6279541ed
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-20 22:45:34 +0530

    refector

--- src/containers/SearchPage.jsx
+++ src/containers/SearchPage.jsx
@@ -1,60 +1,63 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getBeerByName, getBeers } from "../actions";
-import SearchComponent from "../components/SearchComoponent";
-class SearchPage extends Component {
-  state = {
-    beerName: ""
-  };
-  getData = () => {
-    if (this.state.beerName.length > 0) {
-      this.props.getBeerByName(this.state.beerName);
-    } else {
-      this.props.getBeers();
-    }
-  }
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value }, () => {
-      const timer = setTimeout(() => {
-        this.getData()
-      }, 5000)
-      return () => { clearTimeout(timer) }
-    });
+/** @format */
 
-  };
-  onKeyUp = e => {
-    if (e.keyCode === 13) {
-      this.buttonClick();
-    }
-  };
-  buttonClick = () => {
-    if (this.state.beerName.length > 0) {
-      this.props.getBeerByName(this.state.beerName);
-    } else {
-      alert("Please Enter Search Term");
-      this.props.getBeers();
-    }
-  };
-  // componentWillUnmount() {
-  //   clearTimeout(timer)
-  // }
-  render() {
-    return (
-      <div className="ui row">
-        <SearchComponent
-          classNameButton="ui primary button"
-          classNameSearch="custom-search"
-          buttonClick={this.buttonClick}
-          handleChange={this.handleChange}
-          onKeyUp={this.onKeyUp}
-        />
-      </div>
-    );
-  }
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getBeerByName, getBeers } from '../actions';
+import SearchComponent from '../components/SearchComoponent';
+class SearchPage extends Component {
+	state = {
+		beerName: ''
+	};
+	getData = () => {
+		if (this.state.beerName.length > 0) {
+			this.props.getBeerByName(this.state.beerName);
+		} else {
+			this.props.getBeers();
+		}
+	};
+	handleChange = e => {
+		this.setState({ [e.target.name]: e.target.value }, () => {
+			const timer = setTimeout(() => {
+				this.getData();
+			}, 5000);
+			return () => {
+				clearTimeout(timer);
+			};
+		});
+	};
+	onKeyUp = e => {
+		if (e.keyCode === 13) {
+			this.buttonClick();
+		}
+	};
+	buttonClick = () => {
+		if (this.state.beerName.length > 0) {
+			this.props.getBeerByName(this.state.beerName);
+		} else {
+			alert('Please Enter Search Term');
+			this.props.getBeers();
+		}
+	};
+	// componentWillUnmount() {
+	//   clearTimeout(timer)
+	// }
+	render() {
+		return (
+			<div className='ui row'>
+				<SearchComponent
+					classNameButton='ui primary button'
+					classNameSearch='custom-search'
+					buttonClick={this.buttonClick}
+					handleChange={this.handleChange}
+					onKeyUp={this.onKeyUp}
+				/>
+			</div>
+		);
+	}
 }
 const mapDispatchToProps = dispatch => ({
-  getBeerByName: data => dispatch(getBeerByName(data)),
-  getBeers: () => dispatch(getBeers())
+	getBeerByName: data => dispatch(getBeerByName(data)),
+	getBeers: () => dispatch(getBeers())
 });
 // const mapStateToProps = ( state ) => {
 //     return {
@@ -62,7 +65,4 @@ const mapDispatchToProps = dispatch => ({
 
 //     }
 // }
-export default connect(
-  null,
-  mapDispatchToProps
-)(SearchPage);
+export default connect(null, mapDispatchToProps)(SearchPage);
