6b1a9f969121013db937393534d3faef52ffd78c
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-21 21:18:59 +0530

    refector

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -1,54 +1,55 @@
 /** @format */
 
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import MainComponent from '../components/MainComponent';
 import { getBeers, addTofavorites, removeFromfavorites } from '../actions';
 import SearchPage from './SearchPage';
 const MainPage = props => {
+	const dispatch = useDispatch();
+	const beers = useSelector(state => state.beerData.beers);
+	const fbeers = useSelector(state => state.beerData.addFbeers);
+	const favorites = useSelector(state => state.beerData.favorites);
 	const getfav = () => {
-		return props.fbeers;
+		return fbeers;
 	};
 	useEffect(() => {
-		if (!props.beers) {
-			props.getBeers(); // call action
+		if (!beers) {
+			dispatch(getBeers()); // call action
 		}
-		getfav();
-	}, [props]);
-	// const getDrivedStateFromProps = () => {
-	// 	getfav();
-	// };
+	}, [beers, dispatch]);
 	const addToFavorite = e => {
-		props.addTofavorites(e.target.id);
+		dispatch(addTofavorites(e.target.id));
 	};
-	const removeFromfavorites = e => {
-		props.removeFromfavorites(e.target.id);
+	const removeFromfavorite = e => {
+		dispatch(removeFromfavorites(e.target.id));
 	};
 	return (
 		<>
 			<SearchPage />
 			<div className='ui row'>
 				<MainComponent
-					beers={props.beers}
+					beers={beers}
 					fbeers={getfav()}
 					addToFavorite={addToFavorite}
-					removeFromfavorites={removeFromfavorites}
-					className={props.favorites}
+					removeFromfavorite={removeFromfavorite}
+					className={favorites}
 				/>
 			</div>
 		</>
 	);
 };
-const mapDispatchToProps = dispatch => ({
-	getBeers: () => dispatch(getBeers()),
-	addTofavorites: data => dispatch(addTofavorites(data)),
-	removeFromfavorites: data => dispatch(removeFromfavorites(data))
-});
-const mapStateToProps = state => {
-	return {
-		beers: state.beerData.beers,
-		fbeers: state.beerData.addFbeers,
-		favorites: state.beerData.favorites
-	};
-};
-export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
+// const mapDispatchToProps = dispatch => ({
+// 	getBeers: () => dispatch(getBeers()),
+// 	addTofavorites: data => dispatch(addTofavorites(data)),
+// 	removeFromfavorites: data => dispatch(removeFromfavorites(data))
+// });
+// const mapStateToProps = state => {
+// 	return {
+// 		beers: state.beerData.beers,
+// 		fbeers: state.beerData.addFbeers,
+// 		favorites: state.beerData.favorites
+// 	};
+// };
+// export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
+export default MainPage;
