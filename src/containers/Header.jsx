8d138a8468d6a71f2982998e5a0ea3f4e8e87105
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-21 21:28:46 +0530

    refector

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
-import NavigationComponent from '../components/NavigationComponent';
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+/** @format */
 
-class Header extends Component {
-    render() {
-        
-        return (
-           
-                <div className='ui row'>
-                    <NavigationComponent />
-                    <ToastContainer autoClose={2000} />
-                </div>
-              
-           
-        )
-    }
+import React from 'react';
+import NavigationComponent from '../components/NavigationComponent';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Header = () => {
+	return (
+		<div className='ui row'>
+			<NavigationComponent />
+			<ToastContainer autoClose={2000} />
+		</div>
+	);
 };
 export default Header;
