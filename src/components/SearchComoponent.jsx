88a5dd73964b1f64bb1ce967fd1ee933cd7c9145
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-23 22:45:59 +0530

    refector

--- src/components/SearchComoponent.jsx
+++ src/components/SearchComoponent.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+/** @format */
+
+import React from 'react';
 const SearchComoponent = props => {
-  return (
-    <div className="ui category search">
-      <div className="ui icon input">
-        <input
-          className={props.classNameSearch}
-          name="beerName"
-          type="text"
-          placeholder="Search for Beer..."
-          autoComplete="off"
-          onChange={props.handleChange}
-          onKeyUp={props.onKeyUp}
-        />
-        <span className={props.classNameButton} onClick={props.buttonClick}>
-          Search
-        </span>
-      </div>
-    </div>
-  );
+	console.log(props);
+	return (
+		<div className='ui category search'>
+			<div className='ui icon input'>
+				<input
+					className={props.classNameSearch}
+					name='beerName'
+					type='text'
+					placeholder='Search for Beer...'
+					autoComplete='off'
+					onChange={props.handleChange}
+					onKeyUp={props.onKeyUp}
+				/>
+				<span className={props.classNameButton} onClick={props.buttonClick}>
+					Search
+				</span>
+			</div>
+		</div>
+	);
 };
 export default SearchComoponent;
