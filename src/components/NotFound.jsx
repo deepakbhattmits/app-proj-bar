f14db409af837982933980d2d0932b7f51534b20
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-23 22:46:27 +0530

    refector

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+/** @format */
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-  const [hover, setHover] = useState(false);
-  const toggleHover = () => {
-    setHover(!hover);
-  };
-  return (
-    <div className="loading">
-      <h2 className="ui header">OOPS something went wrong...</h2>
-      <Link
-        className={`${hover ? "hovered" : "unhover"}`}
-        to="/home"
-        onMouseEnter={toggleHover}
-        onMouseLeave={toggleHover}
-      >
-        <i className={`icon hand point left ${hover ? "" : "outline"}`} /> Go To
-        Home
-      </Link>
-    </div>
-  );
+	const [hover, setHover] = useState(false);
+	const toggleHover = () => {
+		setHover(!hover);
+	};
+	return (
+		<div className='loading'>
+			<h2 className='ui header'>OOPS something went wrong...</h2>
+			<Link
+				className={`${hover ? 'hovered' : 'unhover'}`}
+				to='/home'
+				onMouseEnter={toggleHover}
+				onMouseLeave={toggleHover}>
+				<i className={`icon hand point left ${hover ? '' : 'outline'}`} /> Go To
+				Home
+			</Link>
+		</div>
+	);
 };
 export default NotFound;
