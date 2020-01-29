b1e62eb7ac296dc7c39054d1525a94e210b1bc2c
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-29 22:30:26 +0530

    refector

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => {
+	console.log('TEST ');
 	const [hover, setHover] = useState(false);
 	const toggleHover = () => {
 		setHover(!hover);
