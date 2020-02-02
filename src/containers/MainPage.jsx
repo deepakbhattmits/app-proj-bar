87bf79d4d88bdcd6963857c333e7f30130c93d38
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-02 19:28:07 +0530

    refector

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -15,7 +15,7 @@ const MainPage = props => {
 	};
 	useEffect(() => {
 		if (!beers) {
-			dispatch(getBeers()); // action call
+			dispatch(getBeers()); // call action
 		}
 	}, [beers, dispatch]);
 	const addToFavorite = e => {
