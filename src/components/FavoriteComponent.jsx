5ec560f51676db71ca7f4e4022e0087b412d9e0b
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-02 19:25:50 +0530

    refector

--- src/components/FavoriteComponent.jsx
+++ src/components/FavoriteComponent.jsx
@@ -4,6 +4,7 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Favorite = props => {
+	console.log('props')
 	const [hover, setHover] = useState(false);
 	const toggleHover = () => {
 		setHover(!hover);
