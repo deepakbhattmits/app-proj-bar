fb50535c4d2ac6d84fa1a97e4380e85fcd675b41
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-13 19:44:24 +0530

    refector

--- src/router/ReactRouter.js
+++ src/router/ReactRouter.js
@@ -1,38 +1,40 @@
-import React, { Component } from "react";
-import { Router, Route, Switch } from "react-router-dom";
-import createBrowserHistory from "../helpers/history";
-import Header from "../containers/Header";
-import Footer from "../containers/Footer";
-import MainPage from "../containers/MainPage";
-import FavoritePage from "../containers/FavoritePage";
-import NotFound from "../components/NotFound";
-import ScrollToTop from "../components/ScrollToTop";
+/** @format */
+
+import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import createBrowserHistory from '../helpers/history';
+import Header from '../containers/Header';
+import Footer from '../containers/Footer';
+import MainPage from '../containers/MainPage';
+import FavoritePage from '../containers/FavoritePage';
+import NotFound from '../components/NotFound';
+import ScrollToTop from '../components/ScrollToTop';
 
 class ReactRouter extends Component {
-  render() {
-    return (
-      <Router history={createBrowserHistory}>
-       <ScrollToTop>
-        <header>
-          <Header />
-        </header>
-      
-          <article>
-            <Switch>
-              <Route exact path="/" component={MainPage} />
-              <Route exact path="/home" component={MainPage} />
-              <Route exact path="/favorite" component={FavoritePage} />
-              <Route exact path="*" component={NotFound} />
-            </Switch>
-          </article>
-        
+	render() {
+		console.log('test');
+		return (
+			<Router history={createBrowserHistory}>
+				<ScrollToTop>
+					<header>
+						<Header />
+					</header>
 
-        <footer>
-          <Footer />
-        </footer>
-        </ScrollToTop>
-      </Router>
-    );
-  }
+					<article>
+						<Switch>
+							<Route exact path='/' component={MainPage} />
+							<Route exact path='/home' component={MainPage} />
+							<Route exact path='/favorite' component={FavoritePage} />
+							<Route exact path='*' component={NotFound} />
+						</Switch>
+					</article>
+
+					<footer>
+						<Footer />
+					</footer>
+				</ScrollToTop>
+			</Router>
+		);
+	}
 }
 export default ReactRouter;
