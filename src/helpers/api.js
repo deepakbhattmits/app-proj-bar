c92aa653f62f3dd1a6bfbf2e0cacceb2b76152b2
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-19 19:50:23 +0530

    refector

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,13 +1,17 @@
-import  axios from 'axios';
+/** @format */
+
+import axios from 'axios';
+// create base url
 export default axios.create({
-    baseURL: `https://api.punkapi.com/v2/`,
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer',
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-        "x-ratelimit-limit": 3600, 
-        "x-ratelimit-remaining": 3587,      
-      }
+	baseURL: `https://api.punkapi.com/v2/`,
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json',
+		Authorization: 'Bearer',
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers':
+			'Origin, X-Requested-With, Content-Type, Accept',
+		'x-ratelimit-limit': 3600,
+		'x-ratelimit-remaining': 3587
+	}
 });
