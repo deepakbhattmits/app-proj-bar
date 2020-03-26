663f1894e5806b72244a1f2d7b1866661be1f605
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-26 19:44:00 +0530

    refector

--- src/components/SearchComoponent.jsx
+++ src/components/SearchComoponent.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 const SearchComoponent = props => {
-	console.log(props);
+	// console.log(props);
 	return (
 		<div className='ui category search'>
 			<div className='ui icon input'>
