217b4d0ab201945b1033060d8617184f54e4be5d
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-29 22:30:06 +0530

    refector

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,7 @@
 
 import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
-// setup
+// setup test
 Enzyme.configure({
 	adapter: new EnzymeAdapter(),
 	disableLifecycleMethods: true
