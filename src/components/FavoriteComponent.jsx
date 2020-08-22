d1c23e9750c63e5473bb46daab086b073b64cef9
deepak bhatt <deepakbhattmits@gmail.com>
2020-08-22 13:20:12 +0530

    Update FavoriteComponent.jsx
    refactor

--- src/components/FavoriteComponent.jsx
+++ src/components/FavoriteComponent.jsx
@@ -4,7 +4,7 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Favorite = (props) => {
-	// console.log('props : ');
+	// console.log('props  : ');
 	const [hover, setHover] = useState(false);
 	const toggleHover = () => {
 		setHover(!hover);
