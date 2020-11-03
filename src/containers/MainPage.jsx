b1d1362c4acbe7956c0952d0acbda228f333edcb
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-03 08:48:54 +0530

    Update MainPage.jsx

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -1,12 +1,11 @@
 /** @format */
-
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import MainComponent from '../components/MainComponent';
 import { getBeers, addTofavorites, removeFromfavorites } from '../actions';
 import SearchPage from './SearchPage';
 const MainPage = (props) => {
-	// console.log(props," ");
 	const dispatch = useDispatch();
 	const [isLoading, setIsLoading] = useState(false);
 	const beers = useSelector((state) => state.beerData.beers);
@@ -38,23 +37,10 @@ const MainPage = (props) => {
 					addToFavorite={addToFavorite}
 					removeFromfavorite={removeFromfavorite}
 					className={favorites}
-					// isLoading={isLoading}
+					isLoading={isLoading}
 				/>
 			</div>
 		</>
 	);
 };
-// const mapDispatchToProps = dispatch => ({
-// 	getBeers: () => dispatch(getBeers()),
-// 	addTofavorites: data => dispatch(addTofavorites(data)),
-// 	removeFromfavorites: data => dispatch(removeFromfavorites(data))
-// });
-// const mapStateToProps = state => {
-// 	return {
-// 		beers: state.beerData.beers,
-// 		fbeers: state.beerData.addFbeers,
-// 		favorites: state.beerData.favorites
-// 	};
-// };
-// export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
 export default MainPage;
