7202954d1b361658b55aa6ecaf6bb7a0d840a263
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-03 08:49:45 +0530

    Update FavoriteComponent.jsx

--- src/components/FavoriteComponent.jsx
+++ src/components/FavoriteComponent.jsx
@@ -1,19 +1,19 @@
 /** @format */
 
-import React, { Fragment, useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Favorite = (props) => {
+const Favorite = ({ fBeers, umarkFav }) => {
 	// console.log('props   : ');
 	const [hover, setHover] = useState(false);
-	const toggleHover = () => {
+	const toggleHover = ({ fBeers, umarkFav }) => {
 		setHover(!hover);
 	};
 	const handleRemove = (e) => {
-		props.umarkFav(e.target.id);
+		umarkFav(e.target.id);
 	};
 	const renderFav = () => {
-		if (Object.values(props?.fBeers).length === 0) {
+		if (!!fBeers && Object.values(fBeers).length === 0) {
 			return (
 				<div className='loading'>
 					<h1 className='ui header'>No any favorite items</h1>
@@ -27,41 +27,44 @@ const Favorite = (props) => {
 				</div>
 			);
 		}
-		return Object.values(props?.fBeers).map((el, i, self) => {
-			return (
-				<div className='card a' key={i}>
-					<div className='ui medium image'>
-						<img
-							className='ui medium image'
-							src={el.image_url}
-							alt='img'
-							style={{ width: 200, height: 200 }}
-						/>
-					</div>
-					<div className='content'>
-						<i
-							id={el.id}
-							className='right floated star icon green'
-							title='Unmark Favorite'
-							onClick={handleRemove}
-						/>
-						<div className='header'>{el.name}</div>
-						<div className='meta'>
-							<label>{el.tagline}</label>
+		return (
+			!!fBeers &&
+			Object.values(fBeers).map((el, i, self) => {
+				return (
+					<div className='card a' key={i}>
+						<div className='ui medium image'>
+							<img
+								className='ui medium image'
+								src={el.image_url}
+								alt='img'
+								style={{ width: 200, height: 200 }}
+							/>
 						</div>
-						<div className='description'>{el.description}</div>
+						<div className='content'>
+							<i
+								id={el.id}
+								className='right floated star icon green'
+								title='Unmark Favorite'
+								onClick={handleRemove}
+							/>
+							<div className='header'>{el.name}</div>
+							<div className='meta'>
+								<label>{el.tagline}</label>
+							</div>
+							<div className='description'>{el.description}</div>
+						</div>
 					</div>
-				</div>
-			);
-		});
+				);
+			})
+		);
 	};
 	return (
-		<Fragment>
+		<>
 			<div className='item'>
 				<h3 className='text-capitalize'> favorite page </h3>
 			</div>
 			<div className='ui link cards favorite'>{renderFav()}</div>
-		</Fragment>
+		</>
 	);
 };
 export default Favorite;
