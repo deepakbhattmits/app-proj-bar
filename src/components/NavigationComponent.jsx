6576f0fae1fd031848ecddd106088621476c4a48
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-03 08:49:37 +0530

    Update NavigationComponent.jsx

--- src/components/NavigationComponent.jsx
+++ src/components/NavigationComponent.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from 'react';
+//import React from 'react';
 // import React, { useRef, useEffect, useCallback } from 'react';
 // import { ReactComponent as CalenderSVG } from '../assets/images/icon-calendar.svg';
 
