f0323e52ffa02b822b9bdf8a3927ccd2a189c7ab
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-14 14:06:31 +0530

    Update Header.jsx

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -4,7 +4,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Header = () => {
-	// console.log('TEST   :');
+	// console.log('TEST  :');
 	return (
 		<div className='ui row'>
 			<NavigationComponent />
