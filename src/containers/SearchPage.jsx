7675707da83924ba25efbd418227d05e9544c5fd
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-19 00:01:52 +0530

    Update SearchPage.jsx

--- src/containers/SearchPage.jsx
+++ src/containers/SearchPage.jsx
@@ -4,7 +4,7 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 import { getBeerByName, getBeers } from '../actions';
 import SearchComponent from '../components/SearchComoponent';
-// todo:functional SearchPage
+//todo:functional SearchPage
 class SearchPage extends Component {
 	state = {
 		beerName: '',
