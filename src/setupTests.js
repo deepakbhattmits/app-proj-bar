f0fe211a5a477174730b688423ec7502fda367a9
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-20 08:33:42 +0530

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,7 @@
 
 import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
-//setup tests
+// setup tests
 Enzyme.configure({
 	adapter: new EnzymeAdapter(),
 	disableLifecycleMethods: true,
